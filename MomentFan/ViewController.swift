622a1f315ada72d1ed99ca1f12cefacf677a561e
Anup Saud <anupsaud10@gmail.com>
2025-01-16 23:00:21 -0500

    initial project structure

---
+++ MomentFan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MomentFan
+//
+//  Created by Anup Saud on 2025-01-16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
